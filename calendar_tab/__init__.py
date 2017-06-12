2281e8dff9d0aa867e2cbb71eed60bac9676c252
wowkalucky <wowkalucky@gmail.com>
2017-06-12 17:50:47 +0300

    Add js and css pipelines auto initialization

--- calendar_tab/__init__.py
+++ calendar_tab/__init__.py
@@ -13,4 +13,34 @@ settings.MAKO_TEMPLATES['main'].extend([
     VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/templates',
 ])
 
-log.warn('MAKO_TEMPLATES["main"]: {}'.format(settings.MAKO_TEMPLATES['main']))
+log.debug('MAKO_TEMPLATES["main"]: {}'.format(settings.MAKO_TEMPLATES['main']))
+
+settings.PIPELINE_CSS.update({
+    'style-calendar-tab': {
+        'source_filenames': [
+            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/css/vendor/scheduler/dhtmlxscheduler.css',
+        ],
+        'output_filename': 'css/calendar-tab.css',
+    }
+})
+
+log.debug('PIPELINE_CSS["style-calendar-tab"]: {}'.format(settings.PIPELINE_CSS["style-calendar-tab"]))
+
+settings.PIPELINE_JS.update({
+    'calendar_tab': {
+        'source_filenames': [
+            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/js/calendar-tab.js',
+        ],
+        'output_filename': 'js/calendar_tab.js',
+    },
+    'calendar_tab_vendor': {
+        'source_filenames': [
+            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/js/vendor/scheduler/_dhtmlxscheduler.js',
+            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/js/vendor/scheduler/dhtmlxscheduler_readonly.js',
+        ],
+        'output_filename': 'js/calendar_tab_vendor.js',
+    },
+})
+
+log.debug('PIPELINE_JS["calendar_tab"]: {}'.format(settings.PIPELINE_JS["calendar_tab"]))
+log.debug('PIPELINE_JS["calendar_tab_vendor"]: {}'.format(settings.PIPELINE_JS["calendar_tab_vendor"]))
