9840461ac6d2bf5424200fdbfb4d8ce4cd13bdcd
wowkalucky <wowkalucky@gmail.com>
2017-06-12 22:00:10 +0300

    Move to relative urls in template/static configuration

--- calendar_tab/__init__.py
+++ calendar_tab/__init__.py
@@ -6,11 +6,10 @@ from path import Path as path
 
 log = logging.getLogger(__name__)
 
-DEFAULT_VENV_ROOT = settings.ENV_ROOT / 'venvs/edxapp'
-VENV_ROOT = path(settings.ENV_TOKENS.get('VENV_ROOT', DEFAULT_VENV_ROOT))
+APP_ROOT = path(__file__).abspath().dirname()
 
 settings.MAKO_TEMPLATES['main'].extend([
-    VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/templates',
+    APP_ROOT / 'templates',
 ])
 
 log.debug('MAKO_TEMPLATES["main"]: {}'.format(settings.MAKO_TEMPLATES['main']))
@@ -18,7 +17,7 @@ log.debug('MAKO_TEMPLATES["main"]: {}'.format(settings.MAKO_TEMPLATES['main']))
 settings.PIPELINE_CSS.update({
     'style-calendar-tab': {
         'source_filenames': [
-            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/css/vendor/scheduler/dhtmlxscheduler.css',
+            APP_ROOT / 'static/calendar_tab/css/vendor/scheduler/dhtmlxscheduler.css',
         ],
         'output_filename': 'css/calendar-tab.css',
     }
@@ -29,14 +28,14 @@ log.debug('PIPELINE_CSS["style-calendar-tab"]: {}'.format(settings.PIPELINE_CSS[
 settings.PIPELINE_JS.update({
     'calendar_tab': {
         'source_filenames': [
-            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/js/calendar-tab.js',
+            APP_ROOT / 'static/calendar_tab/js/calendar-tab.js',
         ],
         'output_filename': 'js/calendar_tab.js',
     },
     'calendar_tab_vendor': {
         'source_filenames': [
-            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/js/vendor/scheduler/_dhtmlxscheduler.js',
-            VENV_ROOT / 'src/edx-calendar-tab/calendar_tab/static/calendar_tab/js/vendor/scheduler/dhtmlxscheduler_readonly.js',
+            APP_ROOT / 'static/calendar_tab/js/vendor/scheduler/_dhtmlxscheduler.js',
+            APP_ROOT / 'static/calendar_tab/js/vendor/scheduler/dhtmlxscheduler_readonly.js',
         ],
         'output_filename': 'js/calendar_tab_vendor.js',
     },
